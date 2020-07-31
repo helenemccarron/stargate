03be61d82c9c72eb4dfdfe36053cd968466d284b
helenemccarron <13dreamscraps@gmail.com>
2020-07-31 09:58:32 -0500

    update packages, readme and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Helene's Stargate`,
+    author: `Helene McCarron`,
+    description: `A portfolio and blog`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `gatsbyjs`,
